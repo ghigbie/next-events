402b206101dc328e44bcfeb1643443cfa1c574dc
ghigbie <gehigbie@gmail.com>
2021-06-14 19:30:25 -0400

    Change filter data page

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,4 +1,17 @@
+import { useRouter } from 'next/router';
+
+
 const FilteredEventPage = () => {
+    const router = useRouter();
+    const filterData = router.query.slug;
+
+    if(filterData){
+        return (<p className='center'>Loading...</p>);
+    }
+
+    const filteredYear = filterData[0];
+    const filteredMonth = filterData[1];
+
     return (
         <div>
             <h1>Filtered Events Page</h1>
